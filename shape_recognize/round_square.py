b046ccce8a17f1c9ac47e4afe45e12c324a860ba
xiedan <xiedan@xiaoshouyi.com>
2018-07-16 16:35:38 +0800

    图像识别（二分类）

---
+++ shape_recognize/round_square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/7/16 15:30
+# @Author  : xiedan
+# @File    : round_square.py
+# 逻辑回归模型
+from sklearn.linear_model import LogisticRegression
+import pandas as pd
+
+train = pd.read_csv('data/train.csv')
+test = pd.read_csv('data/test.csv')
+
+train.drop('id', axis=1, inplace=True)
+test.drop('id', axis=1, inplace=True)
+
+train_labels = train.pop('y')
+
+clf = LogisticRegression()
+clf.fit(train, train_labels)
+
+submit = pd.read_csv('data/sample_submit.csv')
+submit['y'] = clf.predict(test)
+submit.to_csv('data/LR_prediction.csv', index=False)
